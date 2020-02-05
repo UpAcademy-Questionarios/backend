b1db12858787be8fbc046d142b69a2e5d0a96b85
Jorge Antunes <jmpdantunes@gmail.com>
2020-02-05 00:16:20 +0000

    Tentativa de simular academias

--- src/main/java/pt/upacademy/coreFinalProject/models/questionnaire/AccountQuestionnaire.java
+++ src/main/java/pt/upacademy/coreFinalProject/models/questionnaire/AccountQuestionnaire.java
@@ -10,7 +10,8 @@ import pt.upacademy.coreFinalProject.models.core.EntityRoot;
 @Entity
 @NamedQueries({ @NamedQuery(name = AccountQuestionnaire.GET_ALL_ACCOUNTS, query = "SELECT a FROM AccountQuestionnaire a"),
 				@NamedQuery(name = AccountQuestionnaire.GET_ACCOUNT_BY_USERID, query = "SELECT a FROM AccountQuestionnaire a WHERE a.userId = :id"),
-				@NamedQuery(name = AccountQuestionnaire.GET_ACCOUNT_ID_LIST_BY_USERID_LIST, query = "SELECT a.id FROM AccountQuestionnaire a WHERE a.userId IN (:ids)")})
+				@NamedQuery(name = AccountQuestionnaire.GET_ACCOUNT_ID_LIST_BY_USERID_LIST, query = "SELECT a.id FROM AccountQuestionnaire a WHERE a.userId IN (:ids)"),
+})
 
 
 public class AccountQuestionnaire extends EntityRoot {
@@ -19,6 +20,7 @@ public class AccountQuestionnaire extends EntityRoot {
 	public static final String GET_ALL_ACCOUNTS = "getAllAccounts";
 	public static final String GET_ACCOUNT_BY_USERID = "getAccountByUserId";
 	public static final String GET_ACCOUNT_ID_LIST_BY_USERID_LIST = "getAccountListByUserList";
+	public static final String GET_DIFFERENT_ACADEMIES = "getDifferentAcademies";
 	private static final long serialVersionUID = 1L;
 
 	private long userId;
--- src/main/java/pt/upacademy/coreFinalProject/services/questionnaire/AccountQuestionnaireService.java
+++ src/main/java/pt/upacademy/coreFinalProject/services/questionnaire/AccountQuestionnaireService.java
@@ -1,12 +1,20 @@
 package pt.upacademy.coreFinalProject.services.questionnaire;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
+import pt.upacademy.coreFinalProject.models.core.User;
 import pt.upacademy.coreFinalProject.models.questionnaire.AccountQuestionnaire;
 import pt.upacademy.coreFinalProject.repositories.questionnaire.AccountQuestionnaireRepository;
 import pt.upacademy.coreFinalProject.services.core.EntityService;
+import pt.upacademy.coreFinalProject.services.core.UserService;
 
 @RequestScoped
 public class AccountQuestionnaireService extends EntityService<AccountQuestionnaireRepository, AccountQuestionnaire>{
@@ -16,7 +24,8 @@ public class AccountQuestionnaireService extends EntityService<AccountQuestionna
 //		repository.addPendingQuestionnaireToAccount(questionnaireId, accountIds);
 //	}
 	
-	
+	@Inject
+	UserService userService;
 	
 	public AccountQuestionnaire getAccountByUserId(long userId) {
 		long[] userAcademies = new long[] {1, 2}; //Para apagar depois da integração dos projetos
@@ -33,4 +42,34 @@ public class AccountQuestionnaireService extends EntityService<AccountQuestionna
 		return repository.getAccountIdListByUserList(ids);
 	}
 	
+	public List<Long> getDistinctAcademies(){
+		Collection<AccountQuestionnaire>  AllAccounts = get();
+		List<Long> distinctAcademies = new ArrayList<Long>();
+		
+		//List<Long> lala = Arrays.asList(a);
+		for (AccountQuestionnaire account : AllAccounts) {
+			if (!distinctAcademies.contains(account.getUserAcademies()[0])) {
+				distinctAcademies.add(account.getUserAcademies()[0]);
+			}
+			
+		}
+		return distinctAcademies;
+	}
+		
+	public	List<User> getUserFromAcademy(long academy){
+		Collection<AccountQuestionnaire>  AllAccounts = get();
+		//Arrays.stream(AllAccounts).anyMatch(academy::equals);
+		//List<Long>
+		List<Long> distinctAcademies = new ArrayList<Long>();
+		for (AccountQuestionnaire account : AllAccounts) {
+			long[] arr = account.getUserAcademies();
+			List<Long> num = Collections.addAll(distinctAcademies, arr);
+			List<Long> lala = Arrays.stream(arr).collect(Collectors.toList());
+			//if (Arrays.stream(account.getUserAcademies()).anyMatch(academy::equals));
+		}
+		//if (AllAccounts.contains(academy))
+		//Arrays.stream(arr).anyMatch(item::equals);
+	return null;
+	
+	}
 }
